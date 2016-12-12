249c43c70980ccbcbd90b12efcece8b1df0a2252
Denys <denys.zhovnerovychgmail.com>
2016-12-12 21:59:48 +0000

    Added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+  # GET request for / which is homepage
   def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
   def aobut
