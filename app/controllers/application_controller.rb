3300bac7eee8d31c73c417c6ccc986f2caf090c5
Denys <denys.zhovnerovychgmail.com>
2016-12-16 23:00:21 +0000

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  # Whitelist the following form fields so that we can process them, if comming
+  # from a Device sign up form.
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
